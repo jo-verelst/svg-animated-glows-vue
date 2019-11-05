a4e2e0c5da1612e9c607a1d9e3f46cb1e251d993
Jo <vereljo@cronos.be>
2019-11-05 09:22:57 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,79 @@
+<template>
+  <div id="app">
+    <section class="decision-banner" :class="roomClass">
+      <div class="container">
+        <h1>{{header}}</h1>
+        <div class="button-row">
+          <a @click="switchRoom('indoor')">Indoor</a>
+          <a @click="switchRoom('outdoor')">Outdoor</a>
+          <a @click="switchRoom('livingRoom')">Living room</a>
+          <a @click="switchRoom('lightbulbs')">Lightbulbs</a>
+        </div>
+      </div>
+      <Glow class="glow-1" startColor="#00FFFF" stopColor="#FFFF00" />
+      <Glow class="glow-2" startColor="#FF00FF" stopColor="#00FFFF" />
+      <Glow class="glow-3" startColor="#FFFFFF" stopColor="#FFFF00" />
+    </section>
+    <section class="products">
+      <div class="container">
+        <div class="product-card"></div>
+        <div class="product-card"></div>
+        <div class="product-card"></div>
+        <div class="product-card"></div>
+        <div class="product-card"></div>
+        <div class="product-card"></div>
+        <div class="product-card"></div>
+        <div class="product-card"></div>
+        <div class="product-card"></div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+import Glow from "./components/Glow.vue";
+
+export default {
+  name: "app",
+  components: {
+    Glow
+  },
+  data() {
+    return {
+      roomClass: '',
+      header: '',
+      productPresets: {
+        indoor: {
+          header: 'Indoor',
+          class: 'indoor'
+        },
+        outdoor: {
+          header: 'Outdoor',
+          class: 'outdoor'
+        },
+        livingRoom: {
+          header: 'Living Room',
+          class: 'living-room'
+        },
+        lightbulbs: {
+          header: 'Lightbulbs',
+          class: 'lightbulbs'
+        }
+      }
+    };
+  },
+  mounted() {
+    this.roomClass = this.productPresets['indoor'].class;
+    this.header = this.productPresets['indoor'].header;
+  },
+  methods: {
+    switchRoom(productPresetKey) {
+      this.roomClass = this.productPresets[productPresetKey].class;
+      this.header = this.productPresets[productPresetKey].header;
+    }
+  }
+};
+</script>
+<style lang="scss">
+#app {}
+</style>
---
+++ src/components/Glow.vue
@@ -0,0 +1,56 @@
+<template>
+    <svg
+      class="svg-glow"
+      data-name="Layer 1"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlns:xlink="http://www.w3.org/1999/xlink"
+      width="400"
+      height="400"
+      viewBox="0 0 400 400"
+    >
+      <defs>
+        <radialGradient
+          :id="`radial-gradient${id}`"
+          cx="200"
+          cy="200"
+          r="200"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop offset="0" stop-color="#fff" />
+          <stop offset="1" />
+        </radialGradient>
+        <mask :id="`mask${id}`" x="0" y="0" width="400" height="400" maskUnits="userSpaceOnUse">
+          <circle cx="200" cy="200" r="200" :style="`fill: url(#radial-gradient${id})`" />
+        </mask>
+        <linearGradient
+          :id="`linear-gradient${id}`"
+          x1="336.6"
+          y1="436.6"
+          x2="63.4"
+          y2="-36.6"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop class="stop" offset="0.18" :stop-color="stopColor" />
+          <stop class="start" offset="0.84" :stop-color="startColor" />
+        </linearGradient>
+      </defs>
+      <title>svg-gradient</title>
+      <g :style="`mask: url(#mask${id})`">
+        <rect width="400" height="400" :style="`fill: url(#linear-gradient${id})`" />
+      </g>
+    </svg>
+</template>
+<script>
+export default {
+  name: "Glow",
+  props: {
+    startColor: String,
+    stopColor: String
+  },
+  data() {
+    return {
+      id: Math.floor(Math.random() * Math.floor(10000000))
+    }
+  },
+};
+</script>
